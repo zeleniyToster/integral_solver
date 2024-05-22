977f8c70ce11ae52cf1e14b6174425e28810ee84
Ivan <demon_cuc@mail.ru>
2024-05-22 08:00:24 +0400

    Добавил проект для тестирования решателя интегралов

---
+++ TestIntegralSolver/FileReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIntegralSolver
+{
+  internal class FileReader
+  {
+    public string filePath {  get; private set; }
+
+    public FileReader(string filePath)
+    {
+      this.filePath = filePath;
+    }
+
+    public string[] Read()
+    {
+      string[] fileContent;
+      using (StreamReader streamReader = new StreamReader(this.filePath))
+      {
+        fileContent = streamReader.ReadToEnd().Split("\n");
+      }
+
+      return fileContent;
+    }
+  }
+}
---
+++ TestIntegralSolver/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestIntegralSolver
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите путь до файла:");
+
+      var fileReader = new FileReader(args[0]);
+      var fileContent = fileReader.Read();
+
+    }
+  }
+}
